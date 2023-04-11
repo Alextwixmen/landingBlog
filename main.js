ec4c79ebdf0e258c2390e40825c953ce51adb30f
Alexander <alexanderlysenko99@mail.ru>
2023-04-11 22:20:31 +0300

    scroll-to-top button

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ document.querySelector('.burger').addEventListener('click', function () {
   this.classList.toggle('active');
   document.querySelector('.menu').classList.toggle('open');
 });
+
+document.querySelector('.uppward').addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
