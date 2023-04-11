46a850cc9c8515693e76ca89b967c032e28e20b1
Alexander <alexanderlysenko99@mail.ru>
2023-04-11 21:08:33 +0300

    tablet adaptive + burger-menu

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 import './src/scss/index.scss';
+document.querySelector('.burger').addEventListener('click', function () {
+  this.classList.toggle('active');
+  document.querySelector('.menu').classList.toggle('open');
+});
