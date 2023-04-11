313dfefa9067bacb9fb83f9f5800250c292b6264
Alexander <alexanderlysenko99@mail.ru>
2023-04-12 01:03:28 +0300

    refactoring

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './src/scss/index.scss';
 document.querySelector('.burger').addEventListener('click', function () {
   this.classList.toggle('active');
   document.querySelector('.menu').classList.toggle('open');
+  document.getElementsByTagName('body')[0].classList.toggle('blockScroll');
 });
 
 document.querySelector('.uppward').addEventListener('click', () => {
@@ -22,15 +23,22 @@ function startAnimation() {
   };
   let observer = new IntersectionObserver(onEntry, options);
   let elements = document.querySelectorAll('.post_animation');
+  let button = document.querySelector('.news-block__button');
   for (let elem of elements) {
     observer.observe(elem);
   }
+  observer.observe(button);
   function onEntry(entry) {
     entry.forEach((elem) => {
-      // console.log(elem.target.classList);
+      if (
+        elem.target.classList[0] === 'news-block__button' &&
+        elem.isIntersecting
+      ) {
+        elem.target.classList.add('news-block__button_show');
+        console.log(elem.target);
+      }
       if (elem.isIntersecting) {
         elem.target.classList.add('element-show');
-        console.log('zopa');
       } else {
         elem.target.classList.remove('element-show');
       }
